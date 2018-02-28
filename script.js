5a595a0fe4c635267489ed172c2e5c2049147fd7
Olivier Devis <devis.o@hotmail.com>
2018-02-28 23:04:16 +0100

    y

--- script.js
+++ script.js
@@ -126,6 +126,14 @@ function initialize() {
   marker.setMap(map);
 }
 
+function initialize() {
+            var latlng = new google.maps.LatLng(50.85045, 4.34878);
+            var myOptions = {
+                zoom: 8,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
  var map = new google.maps.Map(document.getElementById("map_canvas"),
         myOptions);
         }
